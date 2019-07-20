29ceb5219b1a908d8bad44e7344d022d41ad4c60
hero <361754100@qq.com>
2019-07-20 14:48:43 +0800

    完善代码逻辑

--- src/main/java/com/smart/sperms/dao/mapper/SysRoleMenuRelationMapper.java
+++ src/main/java/com/smart/sperms/dao/mapper/SysRoleMenuRelationMapper.java
@@ -1,8 +1,12 @@
 package com.smart.sperms.dao.mapper;
 
+import com.smart.sperms.dao.dto.EquipmentEnableDto;
+import com.smart.sperms.dao.dto.SysMenuDto;
 import com.smart.sperms.dao.model.SysRoleMenuRelation;
 import com.smart.sperms.dao.model.SysRoleMenuRelationExample;
 import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.RowBounds;
 
@@ -22,4 +26,6 @@ public interface SysRoleMenuRelationMapper {
     int updateByExampleSelective(@Param("record") SysRoleMenuRelation record, @Param("example") SysRoleMenuRelationExample example);
 
     int updateByExample(@Param("record") SysRoleMenuRelation record, @Param("example") SysRoleMenuRelationExample example);
+
+    List<SysMenuDto> selectMenuDtoByRoleId(Map<String, Object> params);
 }
